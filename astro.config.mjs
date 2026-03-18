6d517fb79ba6281933e070134a2217cee1d06d89
Jake Bellacera <hi@jakebellacera.com>
2026-03-18 12:20:15 -0700

    Add styling using Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	adapter: vercel({
 		webAnalytics: { enabled: true },
 	}),
+	vite: {
+		plugins: [/** @type {any} */ (tailwindcss())],
+	},
 });
